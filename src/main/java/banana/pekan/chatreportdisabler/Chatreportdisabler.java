e8920bf39f764de82535da31f3ae6f9defb52b10
BananaPekan <bananapekanlinux@gmail.com>
2022-08-01 08:31:43 +0300

    Upload

---
+++ src/main/java/banana/pekan/chatreportdisabler/Chatreportdisabler.java
@@ -0,0 +1,27 @@
+package banana.pekan.chatreportdisabler;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chatreportdisabler extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onChatSend(AsyncPlayerChatEvent event) {
+        event.setMessage(event.getMessage() + " ");
+    }
+
+}
